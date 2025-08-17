e02fd7f1487c8042e13408b6153c921ef925fd58
dreamerdh76 <165673829+dreamerdh76@users.noreply.github.com>
2025-08-17 01:28:09 +0000

    deploy change

--- ms_distribucion/ms_distribucion/settings.py
+++ ms_distribucion/ms_distribucion/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-65guo9h0n$41g6kxpz7#k0ba_4$!_)ncqyud-w6v6pg3jy2(t7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ms-distribucion"]
 
 
 # Application definition
